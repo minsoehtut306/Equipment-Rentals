ec5c981db776fb54ae85b35b526123f43992fc93
minsoehtut306 <minsoehtut306@gmail.com>
2025-09-20 15:36:20 +1200

    Add ReadMe and Clean Up the folder

---
+++ INTRO_USERS/SQL.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace INTRO_USERS
+{
+    //Name:
+    //Student ID:
+    class SQL
+    {
+        //generates the connection to the database       
+        //Make sure that in the Database connection you put your Database connection here:
+        public static SqlConnection con = new SqlConnection(@"Data Source=cairo.cms.waikato.ac.nz;Database=mh1155_INTRO_USERS;Integrated Security=True");
+        public static SqlCommand cmd = new SqlCommand();
+        public static SqlDataReader read;
+
+        /// <summary>
+        /// This excecutres the query, used mainly for 
+        /// insert/delete/update statements etc. where we don't need
+        /// to read from what we are doing.
+        /// </summary>
+        /// <param name="query"></param>
+        public static void executeQuery(string query)
+        {
+            //try catch to catch any unforseen errors gracefully
+            try
+            {
+                con.Close();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                //put a message box in here if you are recieving errors and see if you can find out why?
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Generates an SQL query based on the input
+        /// query e.g. "SELECT * FROM staff"
+        /// </summary>
+        /// <param name="query"></param>
+        public static void selectQuery(string query)
+        {
+            try
+            {
+                con.Close();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = query;
+                read = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                //put a message box in here if you are recieving errors and see if you can find out why?
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Prints out the ID  based on the query givin into a combo box
+        /// </summary>
+        /// <param name="comboBox">A control to be used to write existing names to</param>
+        /// <param name="query">An SQL query to generate from</param>
+        public static void editComboBoxItems(ComboBox comboBox, string query)
+        {
+            bool clear = true;
+
+            //gets data from database
+            SQL.selectQuery(query);
+            //Check that there is something to write brah
+            if (SQL.read.HasRows)
+            {
+                while (SQL.read.Read())
+                {
+                    if (comboBox.Text == SQL.read[0].ToString())
+                    {
+                        clear = false;
+                    }
+                }
+            }
+
+            //gets data from database
+            SQL.selectQuery(query);
+            //if nothing in the comboBox then we need to clear it
+            if (clear)
+            {
+                comboBox.Text = "";
+                comboBox.Items.Clear();
+
+            }
+
+            // this will print whatever is in the database to the combobox
+            if (SQL.read.HasRows)
+            {
+                while (SQL.read.Read())
+                {
+                    comboBox.Items.Add(SQL.read[0].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# INTRO_USERS – Windows Forms Database Application
+
+A simple **C# Windows Forms** project with a **SQL Server** backend.  
+It demonstrates user login, registration, and browsing of session data linked to social media platforms.
+
+---
+
+## Features
+
+- **Login**: authenticate against the `Users` table.  
+- **Register**: add new users to the database.  
+- **Browse**: search user sessions by username and social media.  
+- **Database**: includes `Users`, `SocialMedia`, and `UserSession` tables with sample data.
+
+---
+
+## Language Used
+
+- C# (.NET WinForms)  
+- SQL Server (T-SQL schema + data in `INTRO_USERS.sql`)  
+- ADO.NET (System.Data.SqlClient)
+
+---
+
+## How to Run
+
+1. Create the database in SQL Server using the script in `INTRO_USERS.sql`.  
+2. Open `INTRO_USERS.sln` in Visual Studio.  
+3. Update the connection string in `SQL.cs` if needed.  
+4. Build and run the project.
+
+---
+
+## Notes
+
+- Passwords stored in plain text (demo only).  
+- Queries use string concatenation → vulnerable to SQL injection.  
